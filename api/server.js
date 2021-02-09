84faf24a1743c6a350b2c9005c227a95ce64864f
Lindsay Deaton <lindsay-deaton@lambdastudents.com>
2021-02-09 10:18:29 -0500

    added delete and put with issues

--- api/server.js
+++ api/server.js
@@ -53,3 +53,40 @@ server.post("/api/users/", async (req, res) => {
         }
     }
 })
+
+server.put("api/users/:id", (req, res) => {
+  //what do you need for the update function
+  const { name, bio } = req.body
+  //what items are available in the object that need to be updated?
+  const id = req.params.id
+  //in order to update, you'll need to find something unique for each object.
+  const indexOfUser = users.findIndex(user => user.id === id)
+  
+  try {
+    if (indexOfUser != -1) {
+      user[indexOfUser] = { id, name, bio }
+      //if it does find it, we go to the array and find the index.  the ID then, doesn't change but the name and bio do which were pulled out of the req body.
+      res.status(200).json({id, name, bio})
+    } else {
+      res.status(404).json({message: `No user with id:${id}`})
+   }
+  }
+  catch (e) {
+    res.status(500).json({message: `server error: ${e}`})
+  }
+})
+
+server.delete("/api/users/:id", (req, res) => {
+  const id = req.params.id
+  try {
+    if (!Users.find(user => user.id === id)) {
+      res.status(404).json({ message: `User with id: ${id}, not found.` })
+    } else {
+      users = users.filter(user => user.id !== id)
+      //if the person doesn't have this id, it will go into the .filter, created array.
+      res.status(200).json({ message: `User: ${id} was deleted.` })
+    }
+  } catch (e) {
+    res.status(500).json({message: `Server error: ${e}`})
+  }
+})
